0a92c1165a4b2a9f0690ef5c530a240173961280
Zabien Grochowski <90737625+CSClubUAA@users.noreply.github.com>
2021-09-26 20:18:05 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const Discord =  require('discord.js')
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+
+const prefix = '!'
+
+
+client.once('ready', () => {
+    console.log('GetBetterBot is online!');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase();
+
+    if(command === 'gb')(
+        message.channel.send('https://cdn.discordapp.com/attachments/825506678686351380/891011233896075334/Screenshot_20210916-162804_Discord.jpg')
+    )
+    })
+
+client.login('ODkxODY1NzE5NDMwNTgyMzAy.YVEkrQ.758lxEJ6G2dq_ASWD6q1U7jisd8');
+
